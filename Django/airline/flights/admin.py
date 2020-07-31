d8aa45cc2e1ec1600610defac372028177fb9516
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-07-31 23:41:05 +0200

    Finished SQL MODELS AND MIGRATIONS

--- Django/airline/flights/admin.py
+++ Django/airline/flights/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Airline, Flight
+from .models import Airline, Flight, Passenger
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination","duration")
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airline)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
