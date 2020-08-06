3d820c1ea9574fb30e1eaf927174f4b9bf6cdadf
Aleksandar Stefanovic <a.stefanovic2112@gmail.com>
2020-08-06 23:51:57 +0200

    Created my own Counter

--- JS/counter.js
+++ JS/counter.js
@@ -1,10 +1,36 @@
 let counter = 0;
+let minutes = 0;
+let hours = 0;
 function count(){
     counter++;
-    document.querySelector('h1').innerHTML = counter;
+    document.querySelector('#counter').innerHTML = counter;
+    if(counter==60){
+  
+        min();
+        
+    }
+    if(minutes>60){
+ 
+        hour();
+    
+    }
+}
+
+function min(){
+    minutes++;
+    document.querySelector('#min').innerHTML = minutes;
+    counter=0;
+}
+
+function hour(){
+    hours++;
+    document.querySelector('#hour').innerHTML = hours;
+    min=0;
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').onclick = count;
     setInterval(count,1000);
+ 
+    
 });
